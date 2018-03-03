b49d0f690f944527cd5321c50ec2172df41dc5e4
rbool <randomboolean@gmail.com>
2018-03-03 16:44:16 +0100

    add map.ts

---
+++ src/map.ts
@@ -0,0 +1,28 @@
+enum Planet {
+    Empty = "e",
+    Terra = "r",
+    Desert = "d",
+    Swamp = "s",
+    Oxide = "o",
+    Volcanic = "v",
+    Titanium = "t",
+    Ice = "i",
+    Gaia = "g",
+    Transdim = "m"    
+}
+
+const s1 = "eee,dsee,eeere,eeem,ove"
+const s2 = "evt,eeee,eseie,oeed,eme"
+const s3 = "eem,egee,eeeee,reit,dee"
+const s4 = "eet,ieoe,eveee,eese,eer"
+const s5 = "eei,egee,eeeem,veeo,dee"
+const s5b = "eei,egee,eeeem,veeo,eee"
+const s6 = "eee,esem,eeere,egee,emd"
+const s6b = "eee,eeem,eeere,egee,emd"
+const s7 = "mee,eeos,egeee,eege,tee"
+const s7b = "mee,eege,egeee,eese,tee"
+const s8 = "eer,eeie,eveem,mete,eee"
+const s9 = "eve,eeem,steei,eege,eee"
+const s10 = "eee,edem,reeem,oege,eee"
+
+const sectors = [[s1,s1],[s2,s2],[s3,s3],[s4,s4],[s5,s5b],[s6,s6b],[s7,s7b],[s8,s8],[s9,s9],[s10,s10]]
