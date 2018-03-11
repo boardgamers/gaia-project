550f7f63f650ee36bc3f9dbc02b8a9ef3d91c7e5
coyotte508 <coyotte508@gmail.com>
2018-03-11 17:50:59 +0100

    building configuration

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import Engine from "./src/engine";
 export {Condition, Planet, Resource, Operator} from "./src/enums";
 export {GaiaHex} from "./src/sector";
 
-// Check if the script is being run - not loaded by another module
-if (!module.parent) {
+// Check if the script is being run - not loaded by another module or webpack
+if (!module.parent && !(module as any).webpackPolyfill) {
   // Launch a server on port 9508
   require("./app");
 }
