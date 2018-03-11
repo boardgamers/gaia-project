21a0a54aa78dc29655479e91252d93251e5675e1
coyotte508 <coyotte508@gmail.com>
2018-03-11 16:14:02 +0100

    index.d.ts is actually useless if there is index.ts

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import Engine from "./src/engine";
-
-export {Condition, Planet, Resource, Operator} from "./src/enums";
-export {GaiaHex} from "./src/sector";
-
-export default Engine;
--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import Engine from "./src/engine";
 
+export {Condition, Planet, Resource, Operator} from "./src/enums";
+export {GaiaHex} from "./src/sector";
+
 // Check if the script is being run - not loaded by another module
 if (!module.parent) {
   // Launch a server on port 9508
