edc44c332182550d4948a3acadba6c881c378b7b
coyotte508 <coyotte508@gmail.com>
2018-03-24 22:40:24 +0100

    Restore server capabilities, but fully independent

---
+++ server.ts
@@ -0,0 +1,22 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as morgan from "morgan";
+import Engine from "./src/engine";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(morgan('dev'));
+
+app.post("/", (req, res) => {
+  res.set("Access-Control-Allow-Origin", req.get('origin'));
+  
+  const moves = req.body.moves;
+  const engine = new Engine(moves);
+
+  res.json(engine.data());
+});
+
+app.listen(9508, "localhost");
