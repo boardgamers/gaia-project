8fc7cc0ff3a0b9468b1f0f8f9c595bd747d6dcb1
coyotte508 <coyotte508@gmail.com>
2018-06-04 19:20:13 +0200

    Add more exports

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 import Engine from "./src/engine";
+import Player from "./src/player";
+import PlayerData from "./src/player-data";
+import { FactionBoard } from "./src/faction-boards";
+import Event from "./src/events";
 
 export {Condition, Planet, Resource, Operator, ResearchField, Faction, Command} from "./src/enums";
 export {GaiaHexData} from "./src/sector";
+
+export { Player, PlayerData, Event, FactionBoard };
 
 export default Engine;
