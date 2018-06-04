49e330c5d7797fc580af9de3d39a68227b50eb5a
coyotte508 <coyotte508@gmail.com>
2018-06-04 14:12:55 +0200

    Dismissible alert

--- app/assets/typescript/utils.ts
+++ app/assets/typescript/utils.ts
@@ -1,5 +1,9 @@
 export function showError(error: string) {
-  $("#errors").html(`<div class='alert alert-danger'>${error}</div>`);
+  $("#errors").html(`<div class='alert alert-danger alert-dismissible fade show' role='alert'>${error}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>`);
 }
 
 export function removeError() {
