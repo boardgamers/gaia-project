61625e032a97627a9e524064382bec4b5fd22e34
coyotte508 <coyotte508@gmail.com>
2018-06-04 16:04:10 +0200

    Add new exports

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import Engine from "./src/engine";
 
-export {Condition, Planet, Resource, Operator} from "./src/enums";
+export {Condition, Planet, Resource, Operator, ResearchField, Faction, Command} from "./src/enums";
 export {GaiaHexData} from "./src/sector";
 
 export default Engine;
