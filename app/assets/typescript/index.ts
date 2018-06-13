6bf42b752edc0ec4109e94db4f216740bc53b28f
coyotte508 <coyotte508@gmail.com>
2018-06-13 14:51:35 +0200

    Add graphical interface for choosing number of players

--- app/assets/typescript/index.ts
+++ app/assets/typescript/index.ts
@@ -4,6 +4,8 @@ import "../stylesheets/frontend.scss";
 import { showError, removeError } from "./utils";
 import MapRenderer from "../../renderers/map";
 import ResearchRenderer from "../../renderers/research";
+import AvailableCommand from "@gaia-project/engine/src/available-command";
+import { Command } from "@gaia-project/engine";
 
 const map = new MapRenderer($("canvas#map").get(0) as HTMLCanvasElement);
 const research = new ResearchRenderer($("canvas#research").get(0) as HTMLCanvasElement);
@@ -11,17 +13,22 @@ const research = new ResearchRenderer($("canvas#research").get(0) as HTMLCanvasE
 $("form").on("submit", function(event) {
   event.preventDefault();
 
+  const text = ($("textarea").val() as string).trim(); 
   const data = {
-    moves: ($("textarea").val() as string).split("\n")
+    moves: text ? text.split("\n") : []
   }
 
   $.post("http://localhost:9508/", 
     data,
     data => {
       removeError();
-      map.render(data.map);
-      research.render(data.players);
-      console.log(data)
+
+      if (data.map) {
+        map.render(data.map);
+        research.render(data.players);
+      }
+      
+      showAvailableMoves(data.availableCommands);
     },
     "json"
   ).fail((error, status, exception) => {
@@ -31,4 +38,36 @@ $("form").on("submit", function(event) {
       showError("Error " + error.status + ": " + error.responseText);
     }
   });
-})
+});
+
+$(function() {
+  $("form").triggerHandler("submit");
+});
+
+function showAvailableMoves(commands: AvailableCommand[]) {
+  const $move = $("#move");
+  $move.html("");
+
+  const command = commands[0];
+
+  if (!command || command.name === Command.Init) {
+    for (let i = 2; i <= 5; i++) {
+      const button = $('<button class="btn btn-secondary mr-2">');
+      button.text(`${i} players`);
+      button.attr("command", `init ${i} randomSeed`);
+      $move.append(button);
+    }
+  }
+}
+
+$(document).on("click", "*[command]", function() {
+  const command = $(this).attr("command");
+
+  // Clear exitings list of moves if the game starts anew
+  if (command.startsWith("init")) {
+    $("#moves").val("");
+  }
+
+  $("#moves").val(($("#moves").val() + "\n" + $(this).attr("command")).trim());
+  $("form").triggerHandler("submit");
+});
