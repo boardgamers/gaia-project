90ea747a9aeb5d0236630a1199328aa7f1e03ac7
coyotte508 <coyotte508@gmail.com>
2018-06-13 19:24:00 +0200

    Add AvailableCommand export

--- index.ts
+++ index.ts
@@ -4,10 +4,11 @@ import PlayerData from "./src/player-data";
 import { FactionBoard } from "./src/faction-boards";
 import factions from "./src/factions";
 import Event from "./src/events";
+import AvailableCommand from "./src/available-command";
 
 export {Condition, Planet, Resource, Operator, ResearchField, Faction, Command, Building} from "./src/enums";
 export {GaiaHexData} from "./src/sector";
 
-export { Player, PlayerData, Event, FactionBoard, factions };
+export { Player, PlayerData, Event, FactionBoard, factions, AvailableCommand };
 
 export default Engine;
