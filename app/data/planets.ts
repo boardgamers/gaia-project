56a1c8c82ab19a1065a59a55715ab666befd370f
riccardo.zulian <riccardo.zulian@gmail.com>
2018-06-15 12:12:30 +0200

    correct faction colors

--- app/data/planets.ts
+++ app/data/planets.ts
@@ -1,11 +1,11 @@
 import { Planet } from "@gaia-project/engine";
 
 export default {
-  [Planet.Terra]: {radius: 0.7, color: 0xa25b15, borderColor: 0x874d12},
-  [Planet.Desert]: {radius: 0.7, color: 0xdbc442, borderColor: 0xc1a925},
-  [Planet.Swamp]: {radius: 0.7, color: 0x99ccff, borderColor: 0x3399ff},
-  [Planet.Oxide]: {radius: 0.7, color: 0xff9900, borderColor: 0xff0000},
-  [Planet.Volcanic]: {radius: 0.7, color: 0xff3300, borderColor: 0xff8566},
+  [Planet.Swamp]: {radius: 0.7, color: 0xa25b15, borderColor: 0x874d12},
+  [Planet.Desert]: {radius: 0.7, color: 0xffd700, borderColor: 0xc1a925},
+  [Planet.Terra]: {radius: 0.7, color: 0x99ccff, borderColor: 0x3399ff},
+  [Planet.Volcanic]: {radius: 0.7, color: 0xff9900, borderColor: 0xff0000},
+  [Planet.Oxide]: {radius: 0.7, color: 0xff3300, borderColor: 0xff8566},
   [Planet.Titanium]: {radius: 0.7, color: 0x3d3d5c, borderColor: 0xd1d1e0},
   [Planet.Ice]: {radius: 0.7, color: 0xccffff, borderColor: 0x33ffff},
   [Planet.Gaia]: {radius: 0.7, color: 0x009933, borderColor: 0x004d1a},
