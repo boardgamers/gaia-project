86bc4ad8c359bf79d98323e0796cc6494ef8a7b2
riccardo.zulian <riccardo.zulian@gmail.com>
2018-06-16 11:29:03 +0200

    different academy buildings

--- app/data/building.ts
+++ app/data/building.ts
@@ -3,7 +3,8 @@ import { Building } from "@gaia-project/engine";
 export function buildingName(building: Building): string {
   switch (building) {
     case Building.Mine: return "mine";
-    case Building.Academy: return "academy";
+    case Building.Academy1: return "academy1";
+    case Building.Academy2: return "academy2";
     case Building.TradingStation: return "trading station";
     case Building.ResearchLab: return "research lab";
     case Building.PlanetaryInstitute: return "planetary institute";
