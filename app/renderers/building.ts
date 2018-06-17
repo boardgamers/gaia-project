2d4c4b664242db44c40e0db92b87306d68f32c9f
coyotte508 <coyotte508@gmail.com>
2018-06-17 22:17:32 +0200

    Draw all buidlings
    Fix #3

--- app/renderers/building.ts
+++ app/renderers/building.ts
@@ -7,14 +7,27 @@ export default class BuildingRenderer extends PIXI.Graphics {
 
     this.lineStyle(border*2, 0x303030);
     this.beginFill(factionColor(faction));
-//TODO render different buildings
+
     switch (building) {
       case Building.Mine: {
-        this.drawRect(-0.3*scale, -0.3 * scale, 0.6 * scale, 0.6 * scale);
+        this.drawRect(-0.2*scale, -0.2 * scale, 0.4 * scale, 0.4 * scale);
         break;
       }
       case Building.PlanetaryInstitute: {
-        this.drawRect(-0.3*scale, -0.3 * scale, 0.6 * scale, 0.6 * scale);
+        this.drawRect(-0.4*scale, -0.4 * scale, 0.8 * scale, 0.8 * scale);
+        break;
+      }
+      case Building.TradingStation: {
+        this.drawPolygon([-0.5, Math.sqrt(3)/4, 0.5, Math.sqrt(3)/4, 0, -Math.sqrt(3)/4, -0.5, Math.sqrt(3)/4].map(x => x * scale*0.6));
+        break;
+      }
+      case Building.ResearchLab: {
+        this.drawCircle(0, 0, 0.3*scale);
+        break;
+      }
+      case Building.Academy1:
+      case Building.Academy2: {
+        this.drawCircle(0, 0, 0.5*scale);
         break;
       }
     }
