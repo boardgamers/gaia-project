cc1f1ec8eb17c0039ba70a2bc03ecd24919e496d
riccardo.zulian <riccardo.zulian@gmail.com>
2018-06-28 08:38:42 +0200

    federation tokens

---
+++ src/tiles/federations.ts
@@ -0,0 +1,11 @@
+import { Federation } from "../enums";
+
+export default  {
+  [Federation.Federation1]: ["12vp"],
+  [Federation.Federation2]: ["8vp,q"],
+  [Federation.Federation3]: ["8vp,2t"],
+  [Federation.Federation4]: ["7vp,2o"],
+  [Federation.Federation5]: ["7vp,6c"],
+  [Federation.Federation6]: ["6vp,2k"],
+  [Federation.FederationGleens]: ["o,k,2c"]
+};
