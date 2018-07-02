b6d7b638ab20b149c2cc5cee96234653fd197743
coyotte508 <coyotte508@gmail.com>
2018-07-02 18:19:33 +0200

    Export tiles

--- src/tiles/index.ts
+++ src/tiles/index.ts
@@ -2,10 +2,12 @@ import advancedTechs from "./advanced-techs";
 import techs from "./techs";
 import scoring from "./scoring";
 import boosters from "./boosters";
+import federations from "./federations";
 
 export default {
   advancedTechs,
   techs,
   scoring,
-  boosters
+  boosters,
+  federations
 };
