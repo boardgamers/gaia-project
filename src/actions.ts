ea64636351b351ff094488cc923b91b125c1b72b
coyotte508 <coyotte508@gmail.com>
2018-07-02 20:37:44 +0200

    Change free actions values to be more readable

--- src/actions.ts
+++ src/actions.ts
@@ -2,12 +2,12 @@
 // QIC to extend range is already included in the distance calculation
 
 export default [
-  { cost: "4pw", income: "q" },
-  { cost: "3pw", income: "o" },
-  { cost: "q", income: "o" },
-  { cost: "4pw", income: "k" },
-  { cost: "pw", income: "c" },
-  { cost: "k", income: "c" },
-  { cost: "o", income: "c" },
-  { cost: "o", income: "t" }
+  { cost: "4pw", income: "1q" },
+  { cost: "3pw", income: "1o" },
+  { cost: "1q", income: "1o" },
+  { cost: "4pw", income: "1k" },
+  { cost: "1pw", income: "1c" },
+  { cost: "1k", income: "1c" },
+  { cost: "1o", income: "1c" },
+  { cost: "1o", income: "1t" }
 ];
