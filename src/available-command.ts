9ff2e89fb5ff6561886222a454b894011a2f4597
riccardo.zulian <riccardo.zulian@gmail.com>
2018-07-08 07:15:59 +0200

    fix availability of freeActions for build-mine subPhase

--- src/available-command.ts
+++ src/available-command.ts
@@ -111,6 +111,7 @@ export function generate(engine: Engine): AvailableCommand[] {
             break;
           }
 
+          case Command.Build:
           case Command.EndTurn: {
             commands.push(subCommand);
 
