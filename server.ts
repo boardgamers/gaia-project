ba2038cfae2899d4f085cb5868f029d923c33bc4
coyotte508 <coyotte508@gmail.com>
2018-07-17 22:59:49 +0200

    Simpler errors

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as morgan from "morgan";
+import { AssertionError } from "assert";
 import Engine from "./src/engine";
 
 const app = express();
@@ -12,11 +13,21 @@ app.use(morgan('dev'));
 
 app.post("/", (req, res) => {
   res.set("Access-Control-Allow-Origin", req.get('origin'));
-  const moves = req.body.moves;
-  const engine = new Engine(moves);
-  engine.generateAvailableCommands();
 
-  res.json(engine);
+  try {
+    const moves = req.body.moves;
+    const engine = new Engine(moves);
+    engine.generateAvailableCommands();
+
+    res.json(engine);
+  } catch (err) {
+    console.error(err);
+    if (err instanceof AssertionError) {
+      res.status(422).send(err.message);
+    } else {
+      res.status(500).send("Internal error: " + err.message);
+    }
+  }
 });
 
 app.listen(9508, "localhost", () => {
