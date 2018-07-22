56b42f7e622ab62b114918d2536a0374a23e10e5
coyotte508 <coyotte508@gmail.com>
2018-07-22 06:13:20 +0200

    Show terraforming federation

--- src/components/ResearchTile.vue
+++ src/components/ResearchTile.vue
@@ -2,15 +2,17 @@
   <g :transform="`translate(0, ${y})`" v-b-tooltip.html.left :title="tooltip">
     <rect x="2" y="2" :class='["researchTile", field, {highlighted}]' width=56 :height="height" rx="5" ry="5" @click="onClick" />
     <Token v-for="(player, index) in players" v-if="player.faction && player.data.research[field] == level" :faction="player.faction" :transform="`translate(${tokenX(index)}, ${tokenY(index)})`" :key="player.player" :scale="5" />
+    <FederationTile v-if="this.federation" :federation="this.federation" x="5" y="7" height="35" style="pointer-events: none" />
   </g>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { Component, Prop } from 'vue-property-decorator';
-import { ResearchField, Player } from '@gaia-project/engine';
+import { ResearchField, Player, Federation } from '@gaia-project/engine';
 import { descriptions } from '../data/research';
 import Token from './Token.vue';
+import FederationTile from './FederationTile.vue';
 
 @Component<ResearchTile>({
   computed: {
@@ -25,10 +27,16 @@ import Token from './Token.vue';
     },
     height() {
       return (this.level == 0 || this.level == 5) ? 46 : 36
+    },
+    federation(): Federation {
+      if (this.level == 5 && this.field === ResearchField.Terraforming) {
+        return this.$store.state.game.data.terraformingFederation;
+      }
     }
   },
   components: {
-    Token
+    Token,
+    FederationTile
   }
 })
 export default class ResearchTile extends Vue {
