82c25691348c3372728a9b2e2846123bf7a39861
coyotte508 <coyotte508@gmail.com>
2018-07-30 19:00:11 +0200

    Add a more complicated test for taklons

--- src/faction-boards/taklons.spec.ts
+++ src/faction-boards/taklons.spec.ts
@@ -100,4 +100,45 @@ describe("Taklons", () => {
     expect(engine2.player(Player.Player2).data.brainstone).to.equal(BrainstoneArea.Area2);
     expect(engine2.player(Player.Player2).data.power.gaia).to.equal(6);
   });
+
+  it("should handle a complicated mix of income phase & brainstone area selection", () => {
+    const moves = Engine.parseMoves(`
+      init 2 randomSeed
+      p1 faction terrans
+      p2 faction taklons
+      terrans build m -4x-1
+      taklons build m -3x-2
+      taklons build m -6x3
+      terrans build m -4x2
+      taklons booster booster3
+      terrans booster booster4
+      terrans build ts -4x2.
+      taklons charge 1pw. brainstone area2
+      taklons build ts -6x3.
+      terrans charge 2pw
+      terrans build PI -4x2.
+      taklons charge 2pw
+      taklons build PI -6x3.
+      terrans charge 3pw
+      terrans special step. build m -5x0.
+      taklons charge 1pw,1t. brainstone area3
+      taklons burn 3. action power2. build m -7x3.
+      terrans up gaia.
+      taklons up terra.
+      terrans build gf -3x1.
+      taklons build ts -3x-2.
+      terrans charge 1pw
+      terrans burn 1. spend 3pw for 1o. spend 1q for 1o. build ts -5x0.
+      taklons charge 2pw,1t. brainstone area2
+      taklons burn 1. action power3.
+      terrans pass booster8
+      taklons pass booster5
+      terrans income 4pw
+      taklons income 2pw. brainstone area1. income 4pw. brainstone area3
+      terrans spend 4tg for 1q. spend 1tg for 1c. spend 1tg for 1c
+      terrans build m -3x1.
+    `);
+
+    expect(() => new Engine(moves)).to.not.throw();
+  });
 });
