4dde2d32889c0513f7d9f003dca8ff07f5716cd3
coyotte508 <coyotte508@gmail.com>
2018-08-02 00:03:20 +0200

    Add exports

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import Game from './src/components/Game.vue';
 
 import { gaiaViewer } from './src/store';
+import api, {GameApi} from './src/api';
 
-export {Game, gaiaViewer};
+export {Game, gaiaViewer, api, GameApi};
