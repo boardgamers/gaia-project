457b38a3a5d1e820ba41bd0d650a4853e56210ad
coyotte508 <coyotte508@gmail.com>
2018-08-04 16:55:24 +0200

    Headway in #63

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app">
-    <Navbar/>
+    <Navbar :online="!!gameId" />
     <Alert/>
     <main class="container-fluid">
-      <Game :api="api"/>
+      <div class="text-center" v-if="gameId && freeSpots">
+        <h3>Game '{{gameId}}'</h3>
+        Waiting for {{freeSpots}} player{{freeSpots > 1 ? 's' : ''}} to join...<br/>
+        <form class="form-inline my-2 my-lg-0 mx-auto" method="get" @submit.prevent="joinGame" v-if="canJoin">
+          <div class="input-group mx-auto mt-2">
+            <input class="form-control" type="text" name="name" v-model=name placeholder="Player name" aria-label="Player name" required>
+            <div class="input-group-append">
+              <button class="btn btn-secondary" type="submit">Join</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <Game :api="api" :gameId="gameId" :auth="auth" v-else />
     </main>
   </div>
 </template>
@@ -30,10 +42,39 @@ import * as $ from "jquery";
   computed: {
     api() {
       return api;
+    },
+    gameId() {
+      if (window.location.search.startsWith("?g=")) {
+        return window.location.search.slice("?g=".length);
+      }
+    },
+    auth() {
+      if (!window.localStorage.getItem("auth")) {
+        window.localStorage.setItem("auth", Math.random().toString(36));
+      }
+      return window.localStorage.getItem("auth");
+    },
+    data() {
+      return this.$store.state.gaiaViewer.data;
+    },
+    canJoin() {
+      return !this.data.players.some(pl => pl.auth === this.auth) && this.freeSpots;
+    },
+    freeSpots() {
+      return this.data.players.filter(pl => !pl.auth).length;
     }
   }
 })
 export default class App extends Vue {
+  private name: string = "";
+
+  joinGame() {
+    $.post(`${window.location.protocol}//${window.location.hostname}:9508/g/${this.gameId}/join`, {name: this.name, auth: this.auth}).always(() => api.loadGame(this.gameId).then(data => this.$store.commit('gaiaViewer/receiveData', data)));
+  }
+}
+export default interface App {
+  gameId: string;
+  auth: string;
 }
 </script>
 
