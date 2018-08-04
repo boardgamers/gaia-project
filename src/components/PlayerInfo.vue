405e35202b41f9c7a1eefc9dfa9067a798d0b65e
coyotte508 <coyotte508@gmail.com>
2018-08-04 12:11:17 +0200

    Remove extra <br> when last round

--- src/components/PlayerInfo.vue
+++ src/components/PlayerInfo.vue
@@ -4,7 +4,7 @@
       <b>{{name}}</b> - <span v-b-tooltip.hoover.html="tooltip" >{{faction}}</span> - {{data.victoryPoints}}vp <span v-if="passed">(passed)</span><br/>
       {{data.credits}}c, {{data.ores}}o, {{data.knowledge}}k, {{data.qics}}q, [{{power('gaia')}}] {{power('area1')}}/{{power('area2')}}/{{power('area3')}} pw<br/>
       m: {{data.buildings.m}}/8, ts: {{data.buildings.ts}}/4, lab: {{data.buildings.lab}}/3<span v-if="data.buildings.PI">, PI</span><span v-if="data.buildings.ac1">, ac1</span><span v-if="data.buildings.ac2">, ac2</span>, gf: <span  v-if="data.gaiaformersInGaia>0">[{{data.gaiaformersInGaia}}]</span> {{data.buildings.gf}}/{{data.gaiaformers}}<br/>
-      <span v-if="round<6">Income: {{player.income.replace(/,/g, ', ')}}</span> <br/>
+      <span v-if="round<6">Income: {{player.income.replace(/,/g, ', ')}}<br/></span>
       Range: {{data.range}}, Terraforming cost: {{3 - data.terraformCostDiscount}}o<br/>
       <span v-if="faction === 'Ivits'">Fed value: {{player.progress.structureFedValue }}, No fed value: {{player.progress.structureValue - player.progress.structureFedValue }} <br/></span> 
 
