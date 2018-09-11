36fdc5b9ac21ce8263620c25b2866c767ce78b19
coyotte508 <coyotte508@gmail.com>
2018-09-11 16:31:22 +0200

    Export space map

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ import AvailableCommand from "./src/available-command";
 import tiles from "./src/tiles";
 import Reward from "./src/reward";
 import factionBoards from './src/faction-boards';
+import SpaceMap from './src/map';
 
 export {Condition, Planet, Resource, Operator, ResearchField, Faction, Command, Building, Booster, Federation, BoardAction} from "./src/enums";
 export {TechTile, AdvTechTile, ScoringTile, FinalTile, TechTilePos, AdvTechTilePos, Phase, Round, Player as PlayerEnum} from "./src/enums";
@@ -17,6 +18,6 @@ export { boardActions } from "./src/actions";
 export { FactionBoard, factionBoard } from "./src/faction-boards";
 export {factionBoards};
 
-export { Player, PlayerData, Event, factions, AvailableCommand, tiles, Reward };
+export { Player, PlayerData, Event, factions, AvailableCommand, tiles, Reward, SpaceMap };
 
 export default Engine;
