90d770bdc2b9d4e958483da2bcb24c20b1739b0f
riccardo.zulian <riccardo.zulian@gmail.com>
2018-09-18 14:42:44 +0200

    resource description for nav and terraforming

--- src/components/Resource.vue
+++ src/components/Resource.vue
@@ -6,7 +6,7 @@
     <rect v-else-if="kind=='pw' || kind=='t'" class="power" width="14" height="14" ry="7" rx="7" x="-7" y="-7" />
     <Building v-else-if="kind=='k'" faction="terrans" building="gf" transform="translate(0.5, 0) scale(20)" />
     <Building v-else-if="kind=='gf'" faction="ivits" building="gf" transform="translate(0.5, 0) scale(20)" style="fill: none !important" />
-    <text x="0" y="0" v-if="count && (['o','c','q','k','pw','t','vp','r'].includes(kind) || count === '+')" :class="{plus: count === '+'}">{{label(kind,count,level)}}</text>
+    <text x="0" y="0" v-if="count && (['o','c','q','k','pw','t','vp','r','d'].includes(kind) || count === '+')" :class="{plus: count === '+'}">{{label(kind,count,level)}}</text>
   </g>
 </template>
 
@@ -32,8 +32,17 @@ export default class Resource extends Vue {
   level: number;
 
   label(kind,count,level): string {
+       
+    if (kind == 'd') {
+      return this.level == 2 ? "cost 2" : this.level == 3 ? "cost 1" : "";
+    };
+    if (kind == 'r') {
+      return this.level == 2 ? "nav 2" : this.level == 4 ? "nav 3" : this.level == 5 ? "nav 4" : "";
+    };
+      
     return kind !== 'vp' ? count : '4vp, g>vp';
   }
+
 }
 </script>
 
