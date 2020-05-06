4174f878ef406db6dbaa62e1c6e0d95518de5ec1
Riccardo Zulian <riccardo.zulian@gmail.com>
2020-05-06 19:05:26 +0200

    test for auctions

--- src/engine.spec.ts
+++ src/engine.spec.ts
@@ -777,7 +777,81 @@ describe("auction", () => {
     expect(() => new Engine(moves, {auction: true})).to.throw();
   });
 
+  it ("should throw auction, bid is not in the range", () => {
+    const moves = Engine.parseMoves(`
+        init 2 djfjjv4k
+        p1 faction geodens
+        p2 faction itars
+        p1 bid itars 1
+        p2 bid itars 12
+    `);
 
+    expect(() => new Engine(moves, {auction: true})).to.throw();
+  });
+
+  it ("should support auction and then go to building phase", () => {
+    const moves = Engine.parseMoves(`
+      init 2 randomSeed
+      p1 faction terrans
+      p2 faction geodens
+      p1 bid terrans 1
+      p2 bid terrans 2
+      p1 bid terrans 3
+      p2 bid terrans 4
+      p1 bid geodens 1
+      terrans build m -4x2
+    `);
+
+    expect(() => new Engine(moves, {auction: true})).to.not.throw();
+  });
+
+  it ("should throw because players are bidding in the wrong order", () => {
+    const moves = Engine.parseMoves(`
+      init 4 djfjjv4k
+      p1 faction geodens
+      p2 faction taklons
+      p3 faction itars
+      p4 faction terrans
+      p1 bid itars 1
+      p2 bid terrans 1
+      p3 bid itars 2
+      p4 bid terrans 2
+      p2 bid terrans 3
+    `);
+
+    expect(() => new Engine(moves, {auction: true})).to.throw();
+  });
+
+
+  it ("should allow auction, 4 players, right turn order", () => {
+    const moves = Engine.parseMoves(`
+        init 4 djfjjv4k
+        p1 faction geodens
+        p2 faction taklons
+        p3 faction itars
+        p4 faction terrans
+        p1 bid itars 1
+        p2 bid terrans 1
+        p3 bid itars 2
+        p4 bid terrans 2
+        p1 bid geodens 1
+        p2 bid terrans 3
+        p4 bid taklons 1
+    `);
+
+    const engine = new Engine(moves, {auction: true});
+    expect(engine.setup[0].player).to.equal(0)
+    expect(engine.setup[1].player).to.equal(3)
+    expect(engine.setup[2].player).to.equal(2)
+    expect(engine.setup[3].player).to.equal(1)
+    
+    expect(engine.players[0].faction).to.equal(Faction.Geodens);
+    expect(engine.players[1].faction).to.equal(Faction.Terrans);
+    expect(engine.players[2].faction).to.equal(Faction.Itars);
+    expect(engine.players[3].faction).to.equal(Faction.Taklons);
+    
+  });
+  
 });
 
 function parseMoves(moves: string) {
