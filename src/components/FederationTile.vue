27f2663f41e07ac1e1afce510c8a3b85e9b47ea7
Riccardo Zulian <riccardo.zulian@gmail.com>
2020-06-29 18:20:41 +0200

    improved federation tile

--- src/components/FederationTile.vue
+++ src/components/FederationTile.vue
@@ -13,7 +13,7 @@
           :key=i
           :count=reward.count
           :kind=reward.type
-          :transform="`translate(${i === 2 ? 0 : (i - Math.min(rewards.length - 1, 1)/2) * 18}, ${rewards.length === 1 ? 12.5 : (i === 2 ? -7 : 10)})`" />
+          :transform="`translate(${i === 2 ? 0 : (i - Math.min(rewards.length - 1, 1)/2) * 22}, ${rewards.length === 1 ? 12.5 : (i === 2 ? -7 : 10)})`" />
       </g>
     </g>
   </svg>
