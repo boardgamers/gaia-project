959fafebeb5411451ba2d676dd30e5c8e09f1376
Riccardo Zulian <riccardo.zulian@gmail.com>
2020-06-29 21:59:46 +0200

    Research lab redo

--- src/components/Buildings/ResearchLab.vue
+++ src/components/Buildings/ResearchLab.vue
@@ -1,6 +1,6 @@
 <template>
   <g>
-    <image xlink:href="../../assets/buildings/research-lab.svg" :filter="faction ? `url(#color-${faction})` : ''" width=110 x=-60 y=-55 />
+    <image xlink:href="../../assets/buildings/research-lab.svg" :filter="faction ? `url(#color-${faction})` : ''" width=90 x=-45 y=-45 />
   </g>
 </template>
 <script lang="ts">
