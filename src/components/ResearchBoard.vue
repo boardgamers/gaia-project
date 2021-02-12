5a64a6c75ce933380f74e374760e1318e1423e31
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2021-02-12 21:30:12 +0100

    show free resource conversion

--- src/components/ResearchBoard.vue
+++ src/components/ResearchBoard.vue
@@ -3,14 +3,37 @@
     <ResearchTrack v-for="(field, index) in [...fields].reverse()" :field=field :x="(fields.length - 1 - index)*60" :key="field" />
     <text y="198" x="180" style="font-size: 12px; text-anchor: middle">Charge 3 power</text>
     <g v-if="$store.state.gaiaViewer.data.tiles && $store.state.gaiaViewer.data.tiles.techs['gaia']">
-      <g transform="translate(100, 410) scale(0.95)" >
+      <g transform="translate(60, 410) scale(0.95)" >
         <TechTile pos="free1" x="-30" y="-30" />
       </g>
-      <g transform="translate(180, 410) scale(0.95)" >
+      <g transform="translate(120, 410) scale(0.95)" >
         <TechTile pos="free2" x="-30" y="-30" />
       </g>
-      <g transform="translate(260, 410) scale(0.95)" >
+      <g transform="translate(180, 410) scale(0.95)" >
         <TechTile pos="free3" x="-30" y="-30" />
+      </g>
+      <!--      resource conversion-->
+      <g transform="translate(250, 410) scale(0.95)">
+        <defs>
+          <marker id="arrowhead" markerWidth="10" markerHeight="7"
+                  refX="0" refY="3.5" orient="auto">
+            <polygon points="0 0, 10 3.5, 0 7"/>
+          </marker>
+          <line id="arrow" x1="0" y1="0" x2="50" y2="0" stroke="#000" stroke-width="8" marker-end="url(#arrowhead)"
+                transform="scale(.1)"/>
+        </defs>
+
+        <Resource kind="k" :count="4" transform="translate(-15, -15)" title="Convert 4 power from bowl III into 1 knowledge"/>
+        <Resource kind="q" :count="4" transform="translate(20, -15)"/>
+        <use xlink:href="#arrow" x="33" y="-15"/>
+        <Resource kind="range" :count="2" transform="translate(57, -15) scale(.6)"/>
+        <use xlink:href="#arrow" x="-6" y="15" transform="rotate(90)"/>
+        <use xlink:href="#arrow" x="-6" y="-20" transform="rotate(90)"/>
+        <Resource kind="c" :count="1" transform="translate(-15, 15)"/>
+        <use xlink:href="#arrow" x="-8" y="-15" transform="rotate(180)"/>
+        <Resource kind="o" :count="3" transform="translate(20, 15)"/>
+        <use xlink:href="#arrow" x="33" y="15"/>
+        <Resource kind="t" transform="translate(57, 15)"/>
       </g>
     </g>
   </svg>
