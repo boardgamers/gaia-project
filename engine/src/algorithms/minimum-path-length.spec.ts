a40ca0f3ca86517bb12f537818d88ad425961d7f
coyotte508 <coyotte508@gmail.com>
2021-02-22 00:00:24 +0100

    ✅ (engine) Remove .only in test

--- engine/src/algorithms/minimum-path-length.spec.ts
+++ engine/src/algorithms/minimum-path-length.spec.ts
@@ -3,7 +3,7 @@ import { Hex } from "hexagrid";
 import minimumPathLength from "./minimum-path-length";
 
 describe("minimum path length", () => {
-  it.only("should get 4 with those 4 hexes in that order", () => {
+  it("should get 4 with those 4 hexes in that order", () => {
     const minCost = minimumPathLength([[new Hex(-2, 10)], [new Hex(0, 3)], [new Hex(-2, 7)], [new Hex(-1, 5)]]);
 
     expect(minCost).to.equal(4);
