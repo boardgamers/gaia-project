b7df6fa4ffc593035b6b2e4f07502611fef33ea2
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2021-04-05 15:16:11 +0200

    :sparkles: (viewer) add base tech tiles charts

---
+++ viewer/src/data/tech-tiles.ts
@@ -0,0 +1,13 @@
+import { TechTile } from "@gaia-project/engine";
+
+export const baseTechTileNames = {
+  [TechTile.Tech1]: { name: "o,q", color: "--qic" },
+  [TechTile.Tech2]: { name: "k for planet types", color: "--dig" },
+  [TechTile.Tech3]: { name: "power value 4 for PI / academy", color: "--recent" },
+  [TechTile.Tech4]: { name: "7 VP", color: "--res-vp" },
+  [TechTile.Tech5]: { name: "1o,1pw income", color: "--ore" },
+  [TechTile.Tech6]: { name: "1k,1c income", color: "--res-knowledge" },
+  [TechTile.Tech7]: { name: "3 VP / build mine on gaia", color: "--gaia" },
+  [TechTile.Tech8]: { name: "4c income", color: "--res-credit" },
+  [TechTile.Tech9]: { name: "4pw special action", color: "--res-power" },
+};
