3ca4ab982bcaeb33d8fd9a7c6dd5dd38251ee0ab
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2021-04-05 22:55:32 +0200

    :sparkles: (viewer) augment advanced log with details about booster and tech tiles

--- viewer/src/data/log.ts
+++ viewer/src/data/log.ts
@@ -1,22 +1,19 @@
-import Engine, { AdvTechTilePos, Command, TechTilePos } from "@gaia-project/engine";
+import Engine from "@gaia-project/engine";
 import { boosterNames } from "./boosters";
 import { advancedTechTileNames, baseTechTileNames } from "./tech-tiles";
 
-export function createReplace(data: Engine): { from: string; to: string }[] {
-  return [
-    Object.keys(boosterNames).map((b) => ({
-      from: b,
-      to: boosterNames[b].name,
-    })),
-    TechTilePos.values().map((p) => ({
-      from: `${Command.ChooseTechTile} ${p}`,
-      to: baseTechTileNames[data.tiles.techs[p].tile].name,
-    })),
-    AdvTechTilePos.values().map((p) => ({
-      from: `${Command.ChooseTechTile} ${p}`,
-      to: advancedTechTileNames[data.tiles.techs[p].tile],
-    })),
-  ]
-    .flat()
-    .map((e) => ({ from: e.from, to: `${e.from} (${e.to})` }));
+function addDetails(s: string, details: string): string {
+  return `${s} (${details})`;
+}
+
+export function replaceMove(data: Engine, move: string): string {
+  return move.replace(/\btech [a-z0-9-]+|booster[0-9]+/g, (match) => {
+    if (match.startsWith("booster")) {
+      return addDetails(match, boosterNames[match].name);
+    } else {
+      const tech = match.substr(5);
+      const tile = data.tiles.techs[tech].tile;
+      return addDetails(match, tech.startsWith("adv") ? advancedTechTileNames[tile] : baseTechTileNames[tile].name);
+    }
+  });
 }
