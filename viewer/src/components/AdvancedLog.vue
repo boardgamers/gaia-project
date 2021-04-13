52d285423ad8858b5f5ef1e4d3433dea12f1b75b
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2021-04-13 09:48:27 +0200

    :sparkles: (viewer) explain changes in log / change foreground color of moves not in own turn

--- viewer/src/components/AdvancedLog.vue
+++ viewer/src/components/AdvancedLog.vue
@@ -28,20 +28,10 @@
           <td v-else-if="event.entry.phase === 'endGame'" class="phase-change">End scoring</td>
           <td v-else>{{ event.move || gameData.players[event.entry.player].faction }}</td>
           <td style="width: 1px; white-space: nowrap">
-            <div
-              v-for="(changes, source, i) in event.entry.changes"
-              v-html="source === 'undefined' ? '&nbsp;' : source"
-              :key="i"
-            />
+            <div v-for="(change, i) in event.changes" v-html="change.source" :key="i" />
           </td>
           <td style="width: 1px; white-space: nowrap">
-            <div v-for="(changes, source, i) in event.entry.changes" :key="i">
-              {{
-                Object.keys(changes)
-                  .map((key) => `${changes[key]}${key}`)
-                  .join(", ")
-              }}
-            </div>
+            <div v-for="(change, i) in event.changes" v-html="change.changes" :key="i" />
           </td>
         </tr>
         <tr class="major-event">
@@ -54,16 +44,13 @@
 </template>
 <script lang="ts">
 import Vue from "vue";
-import {replaceMove} from "../data/log";
 import {Component, Prop} from "vue-property-decorator";
-import Engine, {Faction, LogEntry} from "@gaia-project/engine";
-import {factionLogColors, factionLogTextColors, lightFactionLogColors} from "../graphics/utils";
-import {ownTurn} from "../logic/recent";
+import Engine from "@gaia-project/engine";
 import {LogPlacement} from "../data";
+import {HistoryEntry, makeHistory} from "../data/log";
 
 type ShowLog = "all" | "recent";
 
-type HistoryEntry = { move?: string; entry: LogEntry; round: number; turn: number; color: string; textColor: string };
 
 @Component
 export default class AdvancedLog extends Vue {
@@ -88,82 +75,10 @@ export default class AdvancedLog extends Vue {
     return this.$store.state.gaiaViewer.data;
   }
 
-  get history(): Array<HistoryEntry> {
+  get history(): HistoryEntry[] {
     const offset = this.scope == "recent" ? this.$store.getters["gaiaViewer/recentMoves"].index : 0;
 
-    const ret = [];
-    let round = 0;
-    let turn = 1;
-    let turnFactions: Faction[] = [];
-    let advancedLogIndex = 0;
-    let nextLogEntry = this.gameData.advancedLog[advancedLogIndex];
-
-    const bumpLog = () => {
-      advancedLogIndex += 1;
-      nextLogEntry = this.gameData.advancedLog[advancedLogIndex];
-      if (nextLogEntry?.round) {
-        round = nextLogEntry.round;
-        turn = 1;
-        turnFactions = [];
-      }
-    };
-
-    const newEntry = (move: string = null, entry: LogEntry = nextLogEntry) => {
-      let faction: Faction = null;
-      let own = false;
-      if (entry.player != null) {
-        faction = this.gameData.players[entry.player].faction;
-      } else if (move != null) {
-        const command = move.split(" ", 3);
-        faction = command[0] as Faction;
-        own = ownTurn(move);
-      }
-      const color = faction == null ? "white" : own ? factionLogColors[faction] : lightFactionLogColors[faction];
-      const textColor = faction == null || !own ? "black" : factionLogTextColors[faction];
-      if (own && faction != null) {
-        if (turnFactions.includes(faction)) {
-          turn++;
-          turnFactions = [];
-        }
-        turnFactions.push(faction);
-      }
-
-      return ({move: move, round: round, turn: turn, entry: entry, color: color, textColor: textColor});
-    };
-
-    function addEntry(entry: HistoryEntry) {
-      if (advancedLogIndex > offset) {
-        ret.push(entry);
-      }
-    }
-
-    this.gameData.moveHistory.forEach((move, i) => {
-      while (nextLogEntry && (nextLogEntry.move === undefined || nextLogEntry.move < i)) {
-        if (nextLogEntry.player === undefined || !!nextLogEntry.changes) {
-          addEntry(newEntry());
-        }
-        bumpLog();
-      }
-      const entry = newEntry(replaceMove(this.gameData, move), {} as LogEntry);
-      if (nextLogEntry && nextLogEntry.move === i) {
-        entry.entry = nextLogEntry;
-        bumpLog();
-      }
-      addEntry(entry);
-      while (nextLogEntry && nextLogEntry.move === undefined) {
-        if (nextLogEntry.player === undefined || !!nextLogEntry.changes) {
-          addEntry(newEntry());
-        }
-        bumpLog();
-      }
-    });
-
-    if (nextLogEntry && this.currentMove) {
-      addEntry(newEntry(replaceMove(this.gameData, this.currentMove)));
-    }
-
-    ret.reverse();
-    return ret;
+    return makeHistory(this.gameData, offset, this.currentMove);
   }
 };
 </script>
