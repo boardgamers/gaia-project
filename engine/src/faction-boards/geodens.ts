69b5e1ce93b839f6e9845adaf0d8fe7dd40c55b2
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2022-05-22 08:39:42 +0200

    :bug: (engine) geodens colony should grant knowledge

--- engine/src/faction-boards/geodens.ts
+++ engine/src/faction-boards/geodens.ts
@@ -4,16 +4,19 @@ import Player from "../player";
 import Reward from "../reward";
 import { FactionBoardVariants } from "./types";
 
+function gainExtraKnowledge(player: Player, hex: GaiaHex) {
+  if (player.data.hasPlanetaryInstitute() && player.data.isNewPlanetType(hex)) {
+    player.gainRewards([new Reward("3k")], Faction.Geodens);
+  }
+}
+
 const geodens: FactionBoardVariants = {
   faction: Faction.Geodens,
   standard: {
     income: ["3k,4o,15c,q,up-terra", "+o,k"],
     handlers: {
-      [`build-${Building.Mine}`]: (player: Player, hex: GaiaHex) => {
-        if (player.data.hasPlanetaryInstitute() && player.data.isNewPlanetType(hex)) {
-          player.gainRewards([new Reward("3k")], Faction.Geodens);
-        }
-      },
+      [`build-${Building.Mine}`]: (player: Player, hex: GaiaHex) => gainExtraKnowledge(player, hex),
+      [`build-${Building.Colony}`]: (player: Player, hex: GaiaHex) => gainExtraKnowledge(player, hex),
     },
   },
   variants: [
