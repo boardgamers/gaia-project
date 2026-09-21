b2e88ecb266708166069a288300a7b637d0a7d88
coyotte508 <coyotte508@protonmail.com>
2026-09-21 16:06:23 +0200

    Hide live charge settings and chat in platform analyses

--- viewer/src/components/Game.vue
+++ viewer/src/components/Game.vue
@@ -68,7 +68,7 @@
           <Commands
             v-if="setupActionsAtTop && showCommands"
             :actions-enabled="canPlay"
-            :auto-charge-enabled="!replayData"
+            :auto-charge-enabled="!replayData && !$store.state.preferences.analysis"
             @command="handleCommand"
             :currentMove="currentMove"
             :hide-spacer="true"
@@ -190,7 +190,7 @@
             @command="handleCommand"
             v-if="showCommands && !setupActionsAtTop"
             :actions-enabled="canPlay"
-            :auto-charge-enabled="!replayData"
+            :auto-charge-enabled="!replayData && !$store.state.preferences.analysis"
             :currentMove="currentMove"
             :hide-spacer="true"
             :analysis-mode="analysisMode"
@@ -265,7 +265,7 @@
         @command="handleCommand"
         v-if="showCommands"
         :actions-enabled="canPlay"
-        :auto-charge-enabled="!replayData"
+        :auto-charge-enabled="!replayData && !$store.state.preferences.analysis"
         :currentMove="currentMove"
         :hide-spacer="true"
         @sticky-bar-height="stickyBarHeight = $event"
@@ -296,7 +296,11 @@
       <Table />
       <AdvancedLog :currentMove="currentMove" :hideLog.sync="hideLog" v-if="logPlacement === 'bottom'" />
     </div>
-    <div class="chat-host" :style="{ '--chat-footer-height': totalStickyFooterHeight + 'px' }"></div>
+    <div
+      v-show="!$store.state.preferences.analysis"
+      class="chat-host"
+      :style="{ '--chat-footer-height': totalStickyFooterHeight + 'px' }"
+    ></div>
     <div
       class="mobile-sticky-actions-spacer"
       :style="{ '--sticky-bar-height': totalStickyFooterHeight + 'px' }"
